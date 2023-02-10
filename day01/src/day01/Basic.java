aa761d27c4814647f824ce3699b8b8b1be021b0b
hellohiu <mimi1995@naver.com>
2023-02-11 00:59:34 +0900

    day01 commit
    Java 변수

--- day01/src/day01/Basic.java
+++ day01/src/day01/Basic.java
@@ -3,5 +3,8 @@ package day01;
 public class Basic {
     public static void main(String[] args) {
         System.out.println("안녕하세요");
+        // 주석 : 안녕하세요를 출력하는 메소드입니다.
+        System.out.println("hello world!!");
+        System.out.println("반갑습니다");
     }
 }
---
+++ day01/src/day01/Variable.java
@@ -0,0 +1,13 @@
+package day01;
+
+public class Variable {
+    public static void main(String[] args) {
+        int a;
+        a = 3;
+        int b = 5; // 선언과 동시에 초기화
+        int c, d = 10, e; // 여러개의 변수를 선언
+        
+        System.out.println(a); // 3 출력
+        System.out.println(b); // 5 출력
+    }
+}
