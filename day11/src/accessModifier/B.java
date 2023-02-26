310cf77deaec5d6147b40aa5103a0b4e3490d2c4
hellohiu <mimi1995@naver.com>
2023-02-26 23:17:00 +0900

    day11-2 commit
    getter setter

--- day11/src/accessModifier/B.java
+++ day11/src/accessModifier/B.java
@@ -5,4 +5,14 @@ public class B {
     int var2; // 아무것도 쓰지 않았을 때는 default
     protected int var3;
     private int var4;
+
+    // getter : get + 필드명() -> 저장된 값을 가져오기
+    public int getVar4(){
+        return var4;
+    }
+
+    // setter : set + 필드명() -> 값을 저장하기
+    public void setVar4(int var4){
+        this.var4 = var4;
+    }
 }
--- day11/src/day11/Test.java
+++ day11/src/day11/Test.java
@@ -5,10 +5,15 @@ import accessModifier.B;
 public class Test {
     public static void main(String[] args) {
         B b = new B();
+
+        b.var1 = 3;
         System.out.println(b.var1);
         // public(var1)만 사용 가능
         // protected(var3), default(var2)는 다른 패키지라 사용 불가능
         // private(var4)는 다른 클래스라 사용 불가능
-        
+
+        b.setVar4(4); // (private)var4에 4 저장
+        System.out.println(b.getVar4()); // (private)var4 사용하기
+
     }
 }
